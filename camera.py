1c57584964981327b3dda3b9a06579d6b66cf6c5
haiyang <haiyang@sightai.com>
2023-03-02 13:26:03 +0800

    Initial committed

---
+++ camera.py
@@ -0,0 +1,13 @@
+import cv2
+
+stream = cv2.VideoCapture(0)
+
+while True: 
+    
+    ret, frame = stream.read()
+    image = cv2.flip(frame, 0)
+    cv2.imshow('Video Stream Monitor', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+stream.release()
+cv2.destroyAllWindows()
