802e1b3471d12026bd8743902ebaa6e8f4dcc18e
haiyang <haiyang@sightai.com>
2023-03-06 13:21:35 +0800

    update file extentions

--- torch_yolov5s_read_folder.py
+++ torch_yolov5s_read_folder.py
@@ -1,6 +1,6 @@
 import cv2, torch, time
 import numpy as np
-import glob
+from glob import glob
 
 # Model
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
@@ -8,13 +8,17 @@ model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
 DIRECTORY = 'YOUR-IMAGE-FOLDER'
 
 image_list = []
-for filename in glob.glob(DIRECTORY + '*.png'):
-    print(filename)
+files = glob(DIRECTORY + '*.jpeg')
+files.extend(glob(DIRECTORY + '*.png'))
+files.extend(glob(DIRECTORY + '*.jpg'))
+
+for filename in files:
     results = model([filename])
-    print(results)
-    cv2.imshow('Video Stream Monitor', np.squeeze(results.render()))
+    print(results.pandas().xyxy[0]['name'].tolist())
+    cv2.namedWindow('yolov5', cv2.WINDOW_NORMAL)
+    cv2.imshow('yolov5', np.squeeze(results.render()))
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    time.sleep(0.2)
+    time.sleep(2)
 
 cv2.destroyAllWindows()
