91040780dd7c6afef8e9bca1e699527818bfcb18
haiyang <zhy20090912@gmail.com>
2023-04-06 19:39:46 +0800

    update bird

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,8 @@ WORKDIR /app
 COPY . .
 RUN apt-get update
 RUN apt-get install ffmpeg libsm6 libxext6  -y
+RUN pip3 install wheel
+RUN pip3 install pycairo PyGObject requests pyinstaller
 RUN pip3 install -r requirements.txt
 
 CMD ["python3", "bird_repellen_docker.py"]
--- torch_yolov5s_without_camera.py
+++ torch_yolov5s_without_camera.py
@@ -12,7 +12,6 @@ model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
 stream = cv2.VideoCapture(0)
 while True:
     ret, frame = stream.read()
-    # frame = cv2.flip(frame, 0)
     results = model([frame])
     labels = results.pandas().xyxy[0]['name'].tolist()
     if 'person' in labels:
@@ -22,8 +21,6 @@ while True:
       print(voice)
       playsound('voices/' + voice)
 
-
-    cv2.imshow('Video Stream Monitor', np.squeeze(results.render()))
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
